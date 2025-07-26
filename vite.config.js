94d7fb9d6b1d74812247fc228c62cb5b2daf0abe
shivansh pathania <shivanshpathania27@gmail.com>
2025-07-26 16:52:41 +0530

    df

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ochi/',
+  base: '/Ochi-Design/', 
   plugins: [react(),
     tailwindcss()
   ],
